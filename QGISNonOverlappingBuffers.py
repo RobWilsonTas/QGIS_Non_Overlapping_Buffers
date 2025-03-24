b82ec5e87b7f5f24ca21f62d3a07bebdacd631e5
RobWilsonTas <160975545+RobWilsonTas@users.noreply.github.com>
2025-03-24 12:31:59 +1100

    Update QGISNonOverlappingBuffers.py

--- QGISNonOverlappingBuffers.py
+++ QGISNonOverlappingBuffers.py
@@ -1,19 +1,22 @@
-import datetime
+import time, glob
 from pathlib import Path
-startTime = datetime.time()
+startTime = time.time()
 
 
 """
 ##########################################################
-User options for the tiling section
+User options 
 """
 
 #Initial variable assignment
-polygonLayer1 = 'D:/PublicLand.gpkg' 
-polygonLayer2 = 'D:/PrivateLand.gpkg' 
+polygonLayer = 'D:/PolyLayer.gpkg' 
 
-maximumGap = 50
+identifyingColumn = 'layer'
+
 errorTolerance = 10
+
+gridCellSize = errorTolerance * 400
+
 
 
 """
@@ -23,10 +26,26 @@ errorTolerance = 10
 
 
 #Making a folder for processing
-rootProcessDirectory = str(Path(polygonLayer1).parent.absolute()).replace('\\','/') + '/'
-processDirectory = rootProcessDirectory + 'ProcessBuffer' + '/'
+rootProcessDirectory = str(Path(polygonLayer).parent.absolute()).replace('\\','/') + '/'
+processDirectory = rootProcessDirectory + 'VoronoiProcess' + '/'
 if not os.path.exists(processDirectory): os.mkdir(processDirectory)
 
+tileDirectory = processDirectory + 'Tiles' + '/'
+if not os.path.exists(tileDirectory): os.mkdir(tileDirectory)
+
+outputsDirectory = processDirectory + 'Outputs' + '/'
+if not os.path.exists(outputsDirectory): os.mkdir(outputsDirectory)
+
+
+poly = QgsVectorLayer(polygonLayer)
+polyExtent = poly.extent()
+
+xmin = polyExtent.xMinimum()
+xmax = polyExtent.xMaximum()
+ymin = polyExtent.yMinimum()
+ymax = polyExtent.yMaximum()
+extentCoords = "%f,%f,%f,%f" %(xmin, xmax, ymin, ymax)
+extentCoordsNProj = extentCoords + ' [' + poly.crs().authid() + ']'
 
 """
 ##########################################################
@@ -34,163 +53,79 @@ Looped processing
 """
 
 
-currentBasePolygonLayer1 = polygonLayer1
-currentBasePolygonLayer2 = polygonLayer2
+processing.run("native:creategrid", {'TYPE':2,'EXTENT':extentCoordsNProj,'HSPACING':gridCellSize,'VSPACING':gridCellSize,'HOVERLAY':0,'VOVERLAY':0,'CRS':QgsCoordinateReferenceSystem(poly.crs().authid()),
+    'OUTPUT':processDirectory + 'GridOfTiles.gpkg'})
+        
+processing.run("native:extractbylocation", {'INPUT':processDirectory + 'GridOfTiles.gpkg','PREDICATE':[0],'INTERSECT':polygonLayer,'OUTPUT':processDirectory + 'GridOfTilesExtracted.gpkg'})
 
 
-for currentBufferNumber in range(int(maximumGap / (errorTolerance / 4.1))):
-    
-    processing.run("native:buffer", {'INPUT':currentBasePolygonLayer1,'DISTANCE':errorTolerance / 4.1,'SEGMENTS':5,
-    'END_CAP_STYLE':0,'JOIN_STYLE':0,'MITER_LIMIT':2,'DISSOLVE':False,'SEPARATE_DISJOINT':False,'OUTPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + '.gpkg'})
-    
-    processing.run("native:buffer", {'INPUT':currentBasePolygonLayer2,'DISTANCE':errorTolerance / 4.1,'SEGMENTS':5,
-    'END_CAP_STYLE':0,'JOIN_STYLE':0,'MITER_LIMIT':2,'DISSOLVE':False,'SEPARATE_DISJOINT':False,'OUTPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + '.gpkg'})
-    
-    
-    
-    processing.run("native:difference", {'INPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + '.gpkg',
-    'OVERLAY':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + '.gpkg','OUTPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + 'Safe.gpkg','GRID_SIZE':None})
-    
-    processing.run("native:difference", {'INPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + '.gpkg',
-    'OVERLAY':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + '.gpkg','OUTPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + 'Safe.gpkg','GRID_SIZE':None})
-    
-    
-    processing.run("native:mergevectorlayers", {'LAYERS':[processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + 'Safe.gpkg',currentBasePolygonLayer1],
-    'CRS':None,'OUTPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + 'Merged.gpkg'})
-    
-    processing.run("native:mergevectorlayers", {'LAYERS':[processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + 'Safe.gpkg',currentBasePolygonLayer2],
-    'CRS':None,'OUTPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + 'Merged.gpkg'})
-    
-    
-    
-    processing.run("native:dissolve", {'INPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + 'Merged.gpkg','FIELD':[],'SEPARATE_DISJOINT':False,
-    'OUTPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + 'Dissolved.gpkg'})
-    
-    processing.run("native:dissolve", {'INPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + 'Merged.gpkg','FIELD':[],'SEPARATE_DISJOINT':False,
-    'OUTPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + 'Dissolved.gpkg'})
-    
-    
-    
-    currentBasePolygonLayer1 = processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + 'Dissolved.gpkg'
-    currentBasePolygonLayer2 = processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + 'Dissolved.gpkg'
-    
-
-
-"""
-##########################################################
-Tidy up and snap
-"""
-
-
-processing.run("native:simplifygeometries", {'INPUT':currentBasePolygonLayer1,'METHOD':2,'TOLERANCE':errorTolerance,'OUTPUT':processDirectory + 'PolygonLayer1BuffedSimped.gpkg'})
-
-processing.run("native:fixgeometries", {'INPUT':processDirectory + 'PolygonLayer1BuffedSimped.gpkg','METHOD':1,
-    'OUTPUT':processDirectory + 'PolygonLayer1BuffedSimpedFixed.gpkg'})
-    
-
-processing.run("native:snapgeometries", {'INPUT':processDirectory + 'PolygonLayer1BuffedSimpedFixed.gpkg','REFERENCE_LAYER':polygonLayer1,'TOLERANCE':errorTolerance,'BEHAVIOR':0,
-    'OUTPUT':processDirectory + 'PolygonLayer1BufferSimpedSnapped.gpkg'})
-    
-processing.run("native:fixgeometries", {'INPUT':processDirectory + 'PolygonLayer1BufferSimpedSnapped.gpkg','METHOD':1,
-    'OUTPUT':processDirectory + 'PolygonLayer1BufferSimpedSnappedFixed.gpkg'})
+processing.run("native:splitvectorlayer", {'INPUT':processDirectory + 'GridOfTilesExtracted.gpkg','FIELD':'fid','PREFIX_FIELD':True,'FILE_TYPE':0,'OUTPUT':tileDirectory})
 
 
 
-processing.run("native:snapgeometries", {'INPUT':currentBasePolygonLayer2,'REFERENCE_LAYER':processDirectory + 'PolygonLayer1BufferSimpedSnappedFixed.gpkg','TOLERANCE':errorTolerance,'BEHAVIOR':0,
-    'OUTPUT':processDirectory + 'PolygonLayer2BufferSnapped.gpkg'})
+tileFiles = glob.glob(tileDirectory + '*.gpkg')
 
-processing.run("native:fixgeometries", {'INPUT':processDirectory + 'PolygonLayer2BufferSnapped.gpkg','METHOD':1,
-    'OUTPUT':processDirectory + 'PolygonLayer2BufferSnappedFixed.gpkg'})
+x = 0
+for currentTile in tileFiles:
+
+    x = x + 1
     
-    
-    
-"""
-##########################################################
-Provide a final buffering after snapping
-"""
+    if x > 0: #This can be adjusted to be a different value if you get part the way through and need to resume at a later date
+        
+        subProcessDirectory = processDirectory + 'Tile' + str(x) + '/'
+        if not os.path.exists(subProcessDirectory): os.mkdir(subProcessDirectory)
+        
+        
+        
+        processing.run("native:buffer", {'INPUT':currentTile,'DISTANCE':errorTolerance,'SEGMENTS':5,'END_CAP_STYLE':0,'JOIN_STYLE':0,
+            'MITER_LIMIT':2,'DISSOLVE':False,'SEPARATE_DISJOINT':False,'OUTPUT':subProcessDirectory + 'BufferedTile' + str(x) + '.gpkg'})
+        
+        
+        processing.run("native:clip", {'INPUT':polygonLayer,'OVERLAY':subProcessDirectory + 'BufferedTile' + str(x) + '.gpkg','OUTPUT':subProcessDirectory + 'ClippedPoly' + str(x) + '.gpkg'})
+        
+
+
+        processing.run("native:buffer", {'INPUT':subProcessDirectory + 'ClippedPoly' + str(x) + '.gpkg','DISTANCE':-0.5 * errorTolerance,'SEGMENTS':5,'END_CAP_STYLE':0,'JOIN_STYLE':0,
+            'MITER_LIMIT':2,'DISSOLVE':False,'SEPARATE_DISJOINT':False,'OUTPUT':subProcessDirectory + 'ClippedPoly' + str(x) + 'Inward.gpkg'})
+            
+            
+        processing.run("native:simplifygeometries", {'INPUT':subProcessDirectory + 'ClippedPoly' + str(x) + 'Inward.gpkg','METHOD':0,'TOLERANCE':errorTolerance * 0.5,'OUTPUT':subProcessDirectory + 'ClippedPoly' + str(x) + 'InwardSimp.gpkg'})
+
+
+        tile = QgsVectorLayer(currentTile)
+        tileExtent = tile.extent()
+
+        tilexmin = tileExtent.xMinimum()
+        tilexmax = tileExtent.xMaximum()
+        tileymin = tileExtent.yMinimum()
+        tileymax = tileExtent.yMaximum()
+        tileExtentCoords = "%f,%f,%f,%f" %(tilexmin, tilexmax, tileymin, tileymax)
+        tileExtentCoordsNProj = tileExtentCoords + ' [' + tile.crs().authid() + ']'
+
+        processing.run("native:creategrid", {'TYPE':2,'EXTENT':tileExtentCoordsNProj,'HSPACING':errorTolerance * 0.5,'VSPACING':errorTolerance * 0.5,'HOVERLAY':0,'VOVERLAY':0,'CRS':QgsCoordinateReferenceSystem(poly.crs().authid()),
+            'OUTPUT':subProcessDirectory + 'Grid' + str(x) + '.gpkg'})
+        
+
+
+        processing.run("native:joinbynearest", {'INPUT':subProcessDirectory + 'Grid' + str(x) + '.gpkg','INPUT_2':subProcessDirectory + 'ClippedPoly' + str(x) + 'Inward.gpkg',
+            'FIELDS_TO_COPY':[],'DISCARD_NONMATCHING':False,'PREFIX':'','NEIGHBORS':1,'MAX_DISTANCE':None,'OUTPUT':subProcessDirectory + 'Grid' + str(x) + 'Joined.gpkg'})
+
+
+        processing.run("native:dissolve", {'INPUT':subProcessDirectory + 'Grid' + str(x) + 'Joined.gpkg','FIELD':['layer'],'SEPARATE_DISJOINT':False,'OUTPUT':outputsDirectory + 'Grid' + str(x) + 'JoinedDissolved.gpkg'})
+
+
+
+outputFiles = glob.glob(outputsDirectory + '*.gpkg')
+
+processing.run("native:mergevectorlayers", {'LAYERS':outputFiles,'CRS':None,'OUTPUT':processDirectory + 'VoronoisTogether.gpkg'})
+
+processing.run("native:dissolve", {'INPUT':processDirectory + 'VoronoisTogether.gpkg','FIELD':[identifyingColumn],'SEPARATE_DISJOINT':False,
+        'OUTPUT':processDirectory + 'VoronoisTogetherDissolved.gpkg'})
+        
+processing.runAndLoadResults("native:extractbyextent", {'INPUT':processDirectory + 'VoronoisTogetherDissolved.gpkg','EXTENT':extentCoordsNProj,'CLIP':True,'OUTPUT':processDirectory + 'VoronoisTogetherDissolvedClipped.gpkg'})
     
     
-
-processing.run("native:buffer", {'INPUT':processDirectory + 'PolygonLayer1BufferSimpedSnappedFixed.gpkg','DISTANCE':errorTolerance / 4.1,'SEGMENTS':5,
-'END_CAP_STYLE':0,'JOIN_STYLE':0,'MITER_LIMIT':2,'DISSOLVE':False,'SEPARATE_DISJOINT':False,'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRun.gpkg'})
-    
-processing.run("native:buffer", {'INPUT':processDirectory + 'PolygonLayer2BufferSnappedFixed.gpkg','DISTANCE':errorTolerance / 4.1,'SEGMENTS':5,
-'END_CAP_STYLE':0,'JOIN_STYLE':0,'MITER_LIMIT':2,'DISSOLVE':False,'SEPARATE_DISJOINT':False,'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRun.gpkg'})
-    
-    
-
-processing.run("native:difference", {'INPUT':processDirectory + 'PolygonLayer1Buffer' + str(currentBufferNumber) + '.gpkg',
-'OVERLAY':processDirectory + 'PolygonLayer2BufferFinalRun.gpkg','OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunSafe.gpkg','GRID_SIZE':None})
-
-processing.run("native:difference", {'INPUT':processDirectory + 'PolygonLayer2Buffer' + str(currentBufferNumber) + '.gpkg',
-'OVERLAY':processDirectory + 'PolygonLayer1BufferFinalRun.gpkg','OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunSafe.gpkg','GRID_SIZE':None})
-
-
-processing.run("native:mergevectorlayers", {'LAYERS':[processDirectory + 'PolygonLayer1BufferFinalRunSafe.gpkg',currentBasePolygonLayer1],
-'CRS':None,'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunMerged.gpkg'})
-
-processing.run("native:mergevectorlayers", {'LAYERS':[processDirectory + 'PolygonLayer2BufferFinalRunSafe.gpkg',currentBasePolygonLayer2],
-'CRS':None,'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunMerged.gpkg'})
-
-
-
-processing.run("native:dissolve", {'INPUT':processDirectory + 'PolygonLayer1BufferFinalRunMerged.gpkg','FIELD':[],'SEPARATE_DISJOINT':False,
-'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolved.gpkg'})
-
-processing.run("native:dissolve", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunMerged.gpkg','FIELD':[],'SEPARATE_DISJOINT':False,
-'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolved.gpkg'})
-
-
-    
-"""
-##########################################################
-Final snapping
-"""
-
-processing.run("native:multiparttosingleparts", {'INPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolved.gpkg','OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingle.gpkg'})
-
-processing.run("native:multiparttosingleparts", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolved.gpkg','OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingle.gpkg'})
-
-
-
-processing.run("native:extractbyexpression", {'INPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingle.gpkg','EXPRESSION':'$area > ' + str(errorTolerance ** 2),
-'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFilter.gpkg'})
-
-processing.run("native:extractbyexpression", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingle.gpkg','EXPRESSION':'$area > ' + str(errorTolerance ** 2),
-'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFilter.gpkg'})
-
-
-
-processing.run("native:fixgeometries", {'INPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFilter.gpkg','METHOD':1,
-    'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFixed.gpkg'})
-
-processing.run("native:fixgeometries", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFilter.gpkg','METHOD':1,
-    'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixed.gpkg'})
-
-
-
-processing.run("native:simplifygeometries", {'INPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFixed.gpkg','METHOD':0,'TOLERANCE':errorTolerance/3,
-    'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFixedSimp.gpkg'})
-    
-processing.run("native:simplifygeometries", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixed.gpkg','METHOD':0,'TOLERANCE':errorTolerance/3,
-    'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixedSimp.gpkg'})
-   
-    
-processing.run("native:snapgeometries", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixedSimp.gpkg','REFERENCE_LAYER':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFixedSimp.gpkg',
-    'TOLERANCE':errorTolerance,'BEHAVIOR':1,'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixedSimpSnapped.gpkg'})
-
-processing.run("native:fixgeometries", {'INPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixedSimpSnapped.gpkg','METHOD':1,
-    'OUTPUT':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixedSimpSnappedFixed.gpkg'})
-    
-#############
-  
-    
-    
-processing.run("native:snapgeometries", {'INPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFixedSimp.gpkg','REFERENCE_LAYER':processDirectory + 'PolygonLayer2BufferFinalRunDissolvedSingleFixedSimpSnappedFixed.gpkg',
-    'TOLERANCE':errorTolerance,'BEHAVIOR':2,'OUTPUT':processDirectory + 'PolygonLayer1BufferFinalRunDissolvedSingleFixedSimpSnapped.gpkg'})
-    
-
-    
-
-    
+endTime = time.time()
+totalTime = endTime - startTime
+print("With a grid size of " + str(gridCellSize) + " this took " + str(int(totalTime)) + " seconds")
     
